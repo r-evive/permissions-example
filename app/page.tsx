68f7fa8d10b2ed9a79939fccd4d337c9373a9fa1
Piotr <piotrek596596@gmail.com>
2026-03-30 22:17:26 +0200

    feat: dasboard text adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="space-y-4">
-      <AccessAlert hasAccess={true} requiredPermissions={[]} />
+    <div className="space-y-4 px2 md:px-24">
       <Hint title="PS. Everyone has access to this page!" variant={HintVariant.INFO}>This page is accessible to everyone.
-        <div className="flex flex-col mt-8">
+        <span className="text-red-400 my-8">Navigate through pages and change your role below to see how permissions work!</span>
+        <div className="flex flex-col mt-4">
           <span className="text-center italic"><span className="font-bold text-blue-400">Admin</span> has all permissions.</span>
           <span className="text-center italic"><span className="font-bold text-green-400">User</span> can only view pages.</span>
           <span className="text-center italic"><span className="font-bold text-yellow-400">Custom</span> has permissions set by you.</span>
