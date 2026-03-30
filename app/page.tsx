feb09f65552a5fa6d89bf4e3cf0695522e213ac6
Piotr <piotrek596596@gmail.com>
2026-03-30 22:19:21 +0200

    feat: change greeting text

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="space-y-4 px2 md:px-24">
-      <Hint title="PS. Everyone has access to this page!" variant={HintVariant.INFO}>This page is accessible to everyone.
+      <Hint title="Welcome!" variant={HintVariant.INFO}>This page is accessible to everyone.
         <span className="text-red-400 my-8">Navigate through pages and change your role below to see how permissions work!</span>
         <div className="flex flex-col mt-4">
           <span className="text-center italic"><span className="font-bold text-blue-400">Admin</span> has all permissions.</span>
